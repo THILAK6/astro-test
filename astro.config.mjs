6739baeb063d28ba9b7072cc087d129927a4f8e3
shrikumard <shrikumar.d@thoughtworks.com>
2023-02-23 21:12:09 +0530

    Adds Cloudflare dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import react from "@astrojs/react";
-
 import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
